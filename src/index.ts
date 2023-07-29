60f2a638713d0946db957874bef1769858bcac07
David Maksimov <dmaksi@gmail.com>
2023-07-29 14:58:39 -0500

    set up zoho

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,62 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+import {
+  FileStore,
+  HeaderMap,
+  InitializeBuilder,
+  Levels,
+  LogBuilder,
+  OAuthBuilder,
+  ParameterMap,
+  Records,
+  SDKConfigBuilder,
+  USDataCenter,
+  UserSignature,
+} from '@zohocrm/typescript-sdk-2.1';
+
+(async () => {
+  const user = new UserSignature('test@test.com');
+
+  const logger = new LogBuilder()
+    .level(Levels.INFO)
+    .filePath('./zoho-logs.log')
+    .build();
+
+  const dc = USDataCenter.SANDBOX();
+
+  const sdkConfig = new SDKConfigBuilder()
+    .autoRefreshFields(false)
+    .pickListValidation(true)
+    .build();
+
+  const store = new FileStore('./zoho-tokens.txt');
+
+  const token = new OAuthBuilder()
+    .clientId('')
+    .clientSecret('')
+    .refreshToken('')
+    .redirectURL('http://test.com/webadmin/zoho')
+    .build();
+
+  await new InitializeBuilder()
+    .user(user)
+    .environment(dc)
+    .token(token)
+    .store(store)
+    .SDKConfig(sdkConfig)
+    .logger(logger)
+    .initialize();
+
+  const recordOperations = new Records.RecordOperations();
+
+  const paramInstance = new ParameterMap();
+
+  const headerInstance = new HeaderMap();
+
+  const response = await recordOperations.getRecord(
+    BigInt('4226718000004578107'),
+    'Contacts',
+    paramInstance,
+    headerInstance,
+  );
+
+  console.log('response', response);
+})();
